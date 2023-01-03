6a7b8ee8341bdfe8f2fed9103301aec49d28e5e8
John Dance <guitar.hero.john@gmail.com>
2023-01-03 11:46:53 -0500

    commit tree 6dd9a4de36cd11c2cb47f4b5dac55125216df5ae

---
+++ index.js
@@ -0,0 +1,28 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add() {
+    // we'll fill this in shortly
+  }
+function subtract() {
+    // we'll fill this in shortly
+  }
+function multiply() {
+    // we'll fill this in shortly
+  }
+function divide() {
+    // we'll fill this in shortly
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
